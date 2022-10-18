c2747732f8a416e20eb08440d8c9e0e1187f7340
onticinc <onticinc@gmail.com>
2022-10-18 14:01:57 -0600

    working on api_home view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
 
-# Create your views here.
+import json
+from django.http import JsonResponse
+
+def api_home(request, *args, **kwargs):
+
+    body = request.body #byte string of JSON data
+    data = {}
+    try: 
+        data = json.loads(body) # string of JSON data -> Python Dictionary
+    except:
+        pass
+    print(data.keys())
+
+
+
+    return JsonResponse(data)
--- backend/pyclient/basic.py
+++ backend/pyclient/basic.py
@@ -1,11 +1,11 @@
 import requests
 
-endpoint = "https://httpbin.org/status/200"
-endpoint = "https://httpbin.org/"
+endpoint = "http://localhost:8000/api"
+
 
 get_response = requests.get(endpoint)
 
-print(get_response.text)
+print(get_response.json())
 
 
 
